eda33dc47caf38a509e2df9b7e283ccc57334b29
Tanay Vartak <tanayvartak>
2021-05-02 12:20:01 +0530

    Place order functionality added

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -7,10 +7,6 @@ const orderSchema = mongoose.Schema(
       required: true,
       ref: "User",
     },
-    name: {
-      type: String,
-      required: true,
-    },
     orderItems: [
       {
         name: { type: String, required: true },
@@ -35,10 +31,10 @@ const orderSchema = mongoose.Schema(
       required: true,
     },
     paymentResult: {
-      id: { type: String, required: true },
-      status: { type: String, required: true },
-      update_time: { type: String, required: true },
-      email_address: { type: String, required: true },
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
     },
     taxPrice: {
       type: Number,
